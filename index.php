3abe4371b2b25244bea1f4b3dfdf15b71dcceede
THOMAS  SECONDE  DEVIS <kouamedjakiss766@gmail.com>
2024-10-19 20:54:28 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,46 +4,56 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="indexstyle.css">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
     <div class="bg-image">
-        <img src="cyber.jpg" class = "image">
+        <img src="cyber.jpg" class="image" alt="Background Image">
     </div>
 
-<div class="login">
-<?php
-require('db.php');
-session_start();
+    <div class="login">
+        <?php
+        require('db.php');
+        session_start();
 
-if (isset($_POST['username'])){
+        if (isset($_POST['username'])) {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
 
-    $username = $_REQUEST['username'];
-	$password = ($_REQUEST['password']);
-   
-    $query = "SELECT * FROM `users` WHERE `username`='$username' and `password`='' + '$password'";
-	$result = mysqli_query($conn,$query) or die(mysqli_error($conn));
-	$rows = mysqli_num_rows($result);
-        if($rows==1){
-	    $_SESSION['username'] = $username;
-	    header("Location: start.php");
-         }else{
-	echo "<div class='form'>
-<h3>Username/password is incorrect.</h3>
-<br/>Click here to <a href='index.php'>Login</a></div>";
-	}
-    }else{
-?>
-<div class="form">
-<h1>Log In</h1>
-<form action="" method="post" name="login">
-<input type="text" name="username" placeholder="Username" required /><br>
-<input type="password" name="password" placeholder="Password" required />
-<br>
-<input name="submit" type="submit" value="Login" />
-</form>
-<p>Not registered yet? <a href='registration.php'>Register Here</a></p>
-</div>
-<?php } ?>
+            $stmt = $conn->prepare("SELECT password FROM users WHERE username = ?");
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+            $stmt->store_result();
+
+            if ($stmt->num_rows === 1) {
+                $stmt->bind_result($hashed_password);
+                $stmt->fetch();
+
+                if (password_verify($password, $hashed_password)) {
+                    $_SESSION['username'] = $username;
+                    session_regenerate_id(true); // Regenerate session ID
+                    header("Location: start.php");
+                    exit();
+                } else {
+                    echo "<div class='form'><h3>Invalid login credentials.</h3><br/>Click here to <a href='index.php'>Login</a></div>";
+                }
+            } else {
+                echo "<div class='form'><h3>Invalid login credentials.</h3><br/>Click here to <a href='index.php'>Login</a></div>";
+            }
+
+            $stmt->close();
+        } else {
+        ?>
+            <div class="form">
+                <h1>Log In</h1>
+                <form action="" method="post" name="login">
+                    <input type="text" name="username" placeholder="Username" required /><br>
+                    <input type="password" name="password" placeholder="Password" required /><br>
+                    <input name="submit" type="submit" value="Login" />
+                </form>
+                <p>Not registered yet? <a href='registration.php'>Register Here</a></p>
+            </div>
+        <?php } ?>
+    </div>
 </body>
 </html>
